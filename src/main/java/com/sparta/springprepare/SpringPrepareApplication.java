165e888b1a0065aa44e04e36727808ca8d3dae2c
kimhanshin4 <funold1004@gmail.com>
2023-11-01 10:49:27 +0900

    init commit 스프링 드가자!

---
+++ src/main/java/com/sparta/springprepare/SpringPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrepareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrepareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springprepare/SpringPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrepareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
