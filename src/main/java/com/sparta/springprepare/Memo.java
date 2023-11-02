4ac4a6e3eeaad140d3d6cb9987845c978f4dd5c3
kimhanshin4 <funold1004@gmail.com>
2023-11-02 17:17:25 +0900

    Lombok

--- src/main/java/com/sparta/springprepare/Memo.java
+++ src/main/java/com/sparta/springprepare/Memo.java
@@ -1,12 +1,22 @@
 package com.sparta.springprepare;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Memo {
 
-    private String username;
+    private final String username;
     private String contents;
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        Memo memo = new Memo("funold");
+        System.out.println(memo.getUsername());
+    }
 }
