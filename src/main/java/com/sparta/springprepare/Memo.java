e7e34e228e0259ba04c52f7618306160a1107d65
kimhanshin4 <funold1004@gmail.com>
2023-11-02 16:06:35 +0900

    Annotation 중

---
+++ src/main/java/com/sparta/springprepare/Memo.java
@@ -0,0 +1,12 @@
+package com.sparta.springprepare;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Memo {
+
+    private String username;
+    private String contents;
+}
