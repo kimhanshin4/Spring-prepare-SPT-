fd61633f0ec4d8705406c0bd980da952cdf994e7
kimhanshin4 <funold1004@gmail.com>
2023-11-03 11:26:32 +0900

    Controller 실습중 - 클래스 내 중복 메서드 명은 불가

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,16 +1,42 @@
 package com.sparta.springprepare.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //2. 역할 선언
 public class HelloController { //1. 클래스 생성
 
-    @GetMapping("/api/hello") //3. GetMapping으로 api주소
-    @ResponseBody // 다음강의에서 배움
+    @GetMapping("/api/hello") //3. GetMapping으로 api
+    @ResponseBody // 다음강의에서 배움 // 다음에있는 바디의 hello를 응답해라?
     public String hello() { //4. 메서드 생성. 클래스에서 문자열로 반환 => resources/templetes에서 html파일 존재여부 탐색
-        return "Hello World Again!"; // => Dispatcher가 View name의 정보 전달 "그 html 정보를 찾아 반환 해라"
+        return "Hello World Again! Greetings!"; // => Dispatcher가 View name의 정보 전달 "그 html 정보를 찾아 반환 해라"
     }
 
+    @GetMapping("/api/get")
+    @ResponseBody
+    public String get() {
+        return "GET Method 요청";
+    }
+
+    @PostMapping("/api/post")
+    @ResponseBody
+    public String post() {
+        return "POST Method 요청";
+    }
+
+    @PutMapping("/api/put")
+    @ResponseBody
+    public String put() {
+        return "PUT Method 요청";
+    }
+
+    @DeleteMapping("/api/delete")
+    @ResponseBody
+    public String delete() {
+        return "DELETE Method 요청";
+    }
 }
--- src/main/java/com/sparta/springprepare/controller/HelloControllerPractice.java
+++ src/main/java/com/sparta/springprepare/controller/HelloControllerPractice.java
@@ -1,13 +1,13 @@
-package com.sparta.springprepare.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloControllerPractice {
-
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello World!";
-    }
-}
+//package com.sparta.springprepare.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloControllerPractice {
+//
+//    @GetMapping("/api/hello")
+//    public String hello() {
+//        return "Hello World!";
+//    }
+//}
