b4e12c5376872fa28ec48f4b3234bdec2a18cfe7
kimhanshin4 <funold1004@gmail.com>
2023-11-03 15:47:38 +0900

    동적인 페이지 - html/dynamic = 프로토콜 과정 다시공부하자

--- src/main/java/com/sparta/springprepare/html/HtmlController.java
+++ src/main/java/com/sparta/springprepare/html/HtmlController.java
@@ -1,10 +1,13 @@
 package com.sparta.springprepare.html;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HtmlController {
+
+    private static long visitCount = 0;
 
     @GetMapping("/static-hello") //resources폴더의 해당 path의 파일을 매핑
     public String hello() {
@@ -14,6 +17,13 @@ public class HtmlController {
     @GetMapping("/html/redirect") //해당 path로 redirect(재접근) // 302 - 클라이언트가 추가적인 조치를 취해야 함
     public String htmlStatic() {
         return "redirect:/hello.html";
+    }
+
+    @GetMapping("/html/dynamic")
+    public String htmlDynamic(Model model) {
+        visitCount++;
+        model.addAttribute("visits", visitCount);
+        return "hello-visit";
     }
 
 }
