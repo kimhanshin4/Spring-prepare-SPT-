05b7496d6951db7061faebeb39d0089e5ddaaee2
kimhanshin4 <funold1004@gmail.com>
2023-11-04 00:23:35 +0900

    HTTP 데이터를 객체로 처리하는 방법

--- src/main/java/com/sparta/springprepare/request/RequestController.java
+++ src/main/java/com/sparta/springprepare/request/RequestController.java
@@ -2,8 +2,10 @@ package com.sparta.springprepare.request;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,5 +50,31 @@ public class RequestController {
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+// POST http://localhost:8080/hello/request/form/model
+// Header
+//  Content type: application/x-www-form-urlencoded
+// Body
+//  name=Robbie&age=95
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name,
+            star.age);
+    }
+
+    // [Request sample]
+// POST http://localhost:8080/hello/request/form/json
+// Header
+//  Content type: application/json
+// Body
+//  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name,
+            star.age);
     }
 }
