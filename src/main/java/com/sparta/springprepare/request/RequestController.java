1711dd59182c62bf77d92b2cd35c7315a69539cc
kimhanshin4 <funold1004@gmail.com>
2023-11-03 20:49:04 +0900

    PathVariable & Request Param - 너무 어렵다;

---
+++ src/main/java/com/sparta/springprepare/request/RequestController.java
@@ -0,0 +1,52 @@
+package com.sparta.springprepare.request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello/request")
+public class RequestController {
+
+    @GetMapping("/form/html")
+    public String helloForm() {
+        return "hello-request-form";
+    }
+
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/star/Robbie/age/95
+    @GetMapping("/OhMyGod/{name}/age/{age}") // {필드변수}, 필드클래스명, {필드변수}
+    @ResponseBody
+    public String helloRequestPath(
+        @PathVariable String name, @PathVariable int age) {// 위의 {name}, {age} 부분을 매개인자로 받음
+        return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+// GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
+    @GetMapping("/form/param")
+    @ResponseBody
+    public String helloGetRequestParam(@RequestParam(required = false) String name, int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+//    public String helloGetRequestParam(@RequestParam String name, @RequestParam int age) {
+//        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+//    }
+
+    // [Request sample]
+// POST http://localhost:8080/hello/request/form/param
+// Header
+//  Content type: application/x-www-form-urlencoded
+// Body
+//  name=Robbie&age=95
+    @PostMapping("/form/param")
+    @ResponseBody
+    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+}
---
+++ src/main/java/com/sparta/springprepare/request/Star.java
@@ -0,0 +1,12 @@
+package com.sparta.springprepare.request;
+
+public class Star {
+
+    int age;
+    String name;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
